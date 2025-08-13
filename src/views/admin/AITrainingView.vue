4a6079f43cbb257fb40480b65ac584495df17a52
vmanhhh <108534427+vmanhhh@users.noreply.github.com>
2025-08-13 09:21:58 +0700

    Fix API endpoint for website crawling and adjust button label formatting

--- src/views/admin/AITrainingView.vue
+++ src/views/admin/AITrainingView.vue
@@ -288,7 +288,7 @@ const crawlWebsite = async () => {
 
   try {
     isCrawling.value = true
-    const { data: result } = await axios.post('/crawl', {
+    const { data: result } = await axios.post('/crawl/', {
       url: crawlUrl.value,
       collection: 'temp_crawl',
       max_depth: 1,
@@ -644,7 +644,7 @@ onUnmounted(() => {
                     <Loader2 v-if="isGeneratingTTS" class="h-5 w-5 animate-spin" />
                     <Volume2 v-else class="h-5 w-5" :class="{ 'animate-pulse': isPlaying }" />
                     <span>{{ isGeneratingTTS ? 'Generating...' : isPlaying ? 'Playing...' : 'Test Voice'
-                    }}</span>
+                      }}</span>
                   </n-button>
                 </div>
               </div>
